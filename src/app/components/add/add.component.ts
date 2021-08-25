09bc1a986cf5c83b40601ee520a18cb34f59aed2
Dino Jakir <dino.jakir@eucon.com>
2021-08-25 18:45:19 +0200

    Configuration of Add new excercise

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Muscle {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  selectedMuscle: Muscle | undefined;
+  selectionDisabled: boolean = true;
+  muscles: Muscle[] = [
+    { value: 'neck', viewValue: 'Neck' },
+    { value: 'shoulders', viewValue: 'Shoulders' },
+    { value: 'upper-arms', viewValue: 'Upper Arms' },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
 
+  onSelectMuscle(e: any) {
+    this.selectionDisabled = false;
+  }
   onSaveClick() {}
 }
