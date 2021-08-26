43e975caa0032f3085b068608d564f549189e5b9
Dino Jakir <dino.jakir@eucon.com>
2021-08-26 18:55:27 +0200

    Basic configuration of muscle tree

--- src/app/components/configuration/configuration.component.ts
+++ src/app/components/configuration/configuration.component.ts
@@ -1,70 +1,179 @@
 import { FlatTreeControl } from '@angular/cdk/tree';
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import {
   MatTreeFlatDataSource,
   MatTreeFlattener,
 } from '@angular/material/tree';
+import { BehaviorSubject } from 'rxjs';
 
-interface MuscleNode {
-  name: string;
-  children?: MuscleNode[];
+export class MuscleNode {
+  children: MuscleNode[] = [];
+  item: string = '';
 }
 
-const TREE_DATA: MuscleNode[] = [
-  {
-    name: 'Muscles',
-    children: [
-      {
-        name: 'Neck',
-        children: [{ name: 'Broccoli' }, { name: 'Brussels sprouts' }],
-      },
-      {
-        name: 'Upper Arms',
-        children: [{ name: 'Pumpkins' }, { name: 'Carrots' }],
-      },
-    ],
-  },
-];
+export class TodoItemFlatNode {
+  item: string = '';
+  level: number = 0;
+  expandable: boolean = false;
+}
 
-interface FlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
+const TREE_DATA = {
+  Muscles: {
+    Neck: {
+      Apple: null,
+      Berries: null,
+      Orange: null,
+    },
+    UpperArms: {
+      Apple: null,
+      Berries: null,
+      Orange: null,
+    },
+  },
+};
+
+@Injectable()
+export class MuscleDb {
+  dataChange = new BehaviorSubject<MuscleNode[]>([]);
+
+  get data(): MuscleNode[] {
+    return this.dataChange.value;
+  }
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    const data = this.buildFileTree(TREE_DATA, 0);
+
+    this.dataChange.next(data);
+  }
+
+  buildFileTree(obj: { [key: string]: any }, level: number): MuscleNode[] {
+    return Object.keys(obj).reduce<MuscleNode[]>((accumulator, key) => {
+      const value = obj[key];
+      const node = new MuscleNode();
+      node.item = key;
+
+      if (value != null) {
+        if (typeof value === 'object') {
+          node.children = this.buildFileTree(value, level + 1);
+        } else {
+          node.item = value;
+        }
+      }
+
+      return accumulator.concat(node);
+    }, []);
+  }
+
+  insertItem(parent: MuscleNode, name: string) {
+    if (parent.children) {
+      parent.children.push({ item: name } as MuscleNode);
+      this.dataChange.next(this.data);
+    }
+  }
+
+  updateItem(node: MuscleNode, name: string) {
+    node.item = name;
+    this.dataChange.next(this.data);
+  }
 }
 
 @Component({
-  selector: 'app-add',
+  selector: 'app-configuration',
   templateUrl: './configuration.component.html',
   styleUrls: ['./configuration.component.scss'],
+  providers: [MuscleDb],
 })
-export class ConfigurationComponent {
-  private _transformer = (node: MuscleNode, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-    };
-  };
+export class ConfigurationComponent implements OnInit {
+  flatNodeMap = new Map<TodoItemFlatNode, MuscleNode>();
+  nestedNodeMap = new Map<MuscleNode, TodoItemFlatNode>();
+  newItemName = '';
+  treeControl: FlatTreeControl<TodoItemFlatNode>;
+  treeFlattener: MatTreeFlattener<MuscleNode, TodoItemFlatNode>;
+  dataSource: MatTreeFlatDataSource<MuscleNode, TodoItemFlatNode>;
 
-  treeControl = new FlatTreeControl<FlatNode>(
-    (node) => node.level,
-    (node) => node.expandable
-  );
+  constructor(private _database: MuscleDb) {
+    this.treeFlattener = new MatTreeFlattener(
+      this.transformer,
+      this.getLevel,
+      this.isExpandable,
+      this.getChildren
+    );
+    this.treeControl = new FlatTreeControl<TodoItemFlatNode>(
+      this.getLevel,
+      this.isExpandable
+    );
+    this.dataSource = new MatTreeFlatDataSource(
+      this.treeControl,
+      this.treeFlattener
+    );
 
-  treeFlattener = new MatTreeFlattener(
-    this._transformer,
-    (node) => node.level,
-    (node) => node.expandable,
-    (node) => node.children
-  );
-
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-
-  constructor() {
-    this.dataSource.data = TREE_DATA;
+    _database.dataChange.subscribe((data) => {
+      this.dataSource.data = data;
+    });
   }
 
-  hasChild = (_: number, node: FlatNode) => node.expandable;
+  ngOnInit() {
+    this.treeControl.expand(this.transformer(this.dataSource.data[0], 0));
+  }
+
+  getLevel = (node: TodoItemFlatNode) => node.level;
+
+  isExpandable = (node: TodoItemFlatNode) => node.expandable;
+
+  getChildren = (node: MuscleNode): MuscleNode[] => node.children;
+
+  hasChild = (_: number, _nodeData: TodoItemFlatNode) => _nodeData.expandable;
+
+  hasNoContent = (_: number, _nodeData: TodoItemFlatNode) =>
+    _nodeData.item === '';
+
+  transformer = (node: MuscleNode, level: number) => {
+    const existingNode = this.nestedNodeMap.get(node);
+    const flatNode =
+      existingNode && existingNode.item === node.item
+        ? existingNode
+        : new TodoItemFlatNode();
+    flatNode.item = node.item;
+    flatNode.level = level;
+    flatNode.expandable = !!node.children?.length;
+    this.flatNodeMap.set(flatNode, node);
+    this.nestedNodeMap.set(node, flatNode);
+    return flatNode;
+  };
+
+  getParentNode(node: TodoItemFlatNode): TodoItemFlatNode | null {
+    const currentLevel = this.getLevel(node);
+
+    if (currentLevel < 1) {
+      return null;
+    }
+
+    const startIndex = this.treeControl.dataNodes.indexOf(node) - 1;
+
+    for (let i = startIndex; i >= 0; i--) {
+      const currentNode = this.treeControl.dataNodes[i];
+
+      if (this.getLevel(currentNode) < currentLevel) {
+        return currentNode;
+      }
+    }
+    return null;
+  }
+
+  addNewItem(node: TodoItemFlatNode) {
+    const parentNode = this.flatNodeMap.get(node);
+    this._database.insertItem(parentNode!, '');
+    this.treeControl.expand(node);
+  }
+
+  saveNode(node: TodoItemFlatNode, itemValue: string) {
+    const nestedNode = this.flatNodeMap.get(node);
+    this._database.updateItem(nestedNode!, itemValue);
+  }
 
   onSaveClick() {}
 }
