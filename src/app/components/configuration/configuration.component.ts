5bb9a4c8621f46ae2f8fa72be0c9f8f11be0ac13
Dino Jakir <dino.jakir@eucon.com>
2021-08-30 18:31:15 +0200

    Props configuration

--- src/app/components/configuration/configuration.component.ts
+++ src/app/components/configuration/configuration.component.ts
@@ -11,25 +11,25 @@ import {
 } from "@angular/material/tree";
 import { BehaviorSubject } from "rxjs";
 
-export class MuscleNode {
+export class Node {
   item: string = "";
-  children: MuscleNode[] = [];
+  children: Node[] = [];
 }
 
-export class MuscleFlatNode {
+export class FlatNode {
   item: string = "";
   level: number = 0;
   expandable: boolean = false;
 }
 
-export class MuscleDb {
-  dataChange = new BehaviorSubject<MuscleNode[]>([]);
+export class Db {
+  dataChange = new BehaviorSubject<Node[]>([]);
 
-  get data(): MuscleNode[] {
+  get data(): Node[] {
     return this.dataChange.value;
   }
 
-  deleteItem(parent: MuscleNode | undefined, name: string): void {
+  deleteItem(parent: Node | undefined, name: string): void {
     if (parent && parent.children) {
       parent.children = parent.children.filter((j) => j.item !== name);
       this.dataChange.next(this.data);
@@ -38,9 +38,9 @@ export class MuscleDb {
     }
   }
 
-  insertItem(parent: MuscleNode | null, name: string): void {
+  insertItem(parent: Node | null, name: string): void {
     if (parent && parent.children) {
-      parent.children.push({ item: name, children: [] } as MuscleNode);
+      parent.children.push({ item: name, children: [] } as Node);
       this.dataChange.next(this.data);
     }
 
@@ -50,9 +50,11 @@ export class MuscleDb {
     }
   }
 
-  updateItem(node: MuscleNode, name: string): void {
-    node.item = name;
-    this.dataChange.next(this.data);
+  updateItem(node: Node | undefined, name: string): void {
+    if (node) {
+      node.item = name;
+      this.dataChange.next(this.data);
+    }
   }
 }
 
@@ -62,96 +64,146 @@ export class MuscleDb {
   styleUrls: ["./configuration.component.scss"],
 })
 export class ConfigurationComponent implements OnInit {
-  flatNodeMap = new Map<MuscleFlatNode, MuscleNode>();
-  nestedNodeMap = new Map<MuscleNode, MuscleFlatNode>();
+  musclesFlatNodeMap = new Map<FlatNode, Node>();
+  musclesNestedNodeMap = new Map<Node, FlatNode>();
+  propsFlatNodeMap = new Map<FlatNode, Node>();
+  propsNestedNodeMap = new Map<Node, FlatNode>();
   newItemName = "";
-  treeControl: FlatTreeControl<MuscleFlatNode>;
-  treeFlattener: MatTreeFlattener<MuscleNode, MuscleFlatNode>;
-  dataSource: MatTreeFlatDataSource<MuscleNode, MuscleFlatNode>;
-  muscleDb: MuscleDb;
+  musclesTreeControl: FlatTreeControl<FlatNode>;
+  musclesTreeFlattener: MatTreeFlattener<Node, FlatNode>;
+  musclesDataSource: MatTreeFlatDataSource<Node, FlatNode>;
+  muscleDb: Db;
+  propsTreeControl: FlatTreeControl<FlatNode>;
+  propsTreeFlattener: MatTreeFlattener<Node, FlatNode>;
+  propsDataSource: MatTreeFlatDataSource<Node, FlatNode>;
+  propsDb: Db;
   loadingVisible = false;
   isMusclesLoaded = false;
+  isPropsLoaded = false;
 
   constructor(private db: AngularFirestore) {
-    this.treeFlattener = new MatTreeFlattener(
-      this.transformer,
+    this.musclesTreeFlattener = new MatTreeFlattener(
+      this.musclesTransformer,
       this.getLevel,
       this.isExpandable,
       this.getChildren
     );
-    this.treeControl = new FlatTreeControl<MuscleFlatNode>(
+    this.musclesTreeControl = new FlatTreeControl<FlatNode>(
       this.getLevel,
       this.isExpandable
     );
-    this.dataSource = new MatTreeFlatDataSource(
-      this.treeControl,
-      this.treeFlattener
+    this.musclesDataSource = new MatTreeFlatDataSource(
+      this.musclesTreeControl,
+      this.musclesTreeFlattener
     );
 
-    this.muscleDb = new MuscleDb();
+    this.muscleDb = new Db();
     this.muscleDb.dataChange.subscribe((data) => {
-      this.dataSource.data = data;
+      this.musclesDataSource.data = data;
+    });
+
+    this.propsTreeFlattener = new MatTreeFlattener(
+      this.propsTransformer,
+      this.getLevel,
+      this.isExpandable,
+      this.getChildren
+    );
+    this.propsTreeControl = new FlatTreeControl<FlatNode>(
+      this.getLevel,
+      this.isExpandable
+    );
+    this.propsDataSource = new MatTreeFlatDataSource(
+      this.propsTreeControl,
+      this.propsTreeFlattener
+    );
+
+    this.propsDb = new Db();
+    this.propsDb.dataChange.subscribe((data) => {
+      this.propsDataSource.data = data;
     });
   }
 
   async ngOnInit(): Promise<void> {
-    await this.getData();
+    const muscles: Node[] = await this.getData("Muscles");
+    this.muscleDb.dataChange.next(muscles || []);
     this.isMusclesLoaded = true;
+
+    const props: Node[] = await this.getData("Props");
+    this.propsDb.dataChange.next(props || []);
+    this.isPropsLoaded = true;
   }
 
-  getLevel = (node: MuscleFlatNode) => node.level;
+  getLevel = (node: FlatNode) => node.level;
 
-  isExpandable = (node: MuscleFlatNode) => node.expandable;
+  isExpandable = (node: FlatNode) => node.expandable;
 
-  getChildren = (node: MuscleNode): MuscleNode[] => node.children;
+  getChildren = (node: Node): Node[] => node.children;
 
-  canAdd = (_: number, _nodeData: MuscleFlatNode) => _nodeData.expandable;
+  canAdd = (_: number, _nodeData: FlatNode) => _nodeData.expandable;
 
-  hasNoContent = (_: number, _nodeData: MuscleFlatNode) =>
-    _nodeData.item === "";
+  hasNoContent = (_: number, _nodeData: FlatNode) => _nodeData.item === "";
 
-  transformer = (node: MuscleNode, level: number) => {
-    const existingNode: MuscleFlatNode | undefined =
-      this.nestedNodeMap.get(node);
-    const flatNode: MuscleFlatNode =
+  musclesTransformer = (node: Node, level: number) => {
+    const existingNode: FlatNode | undefined =
+      this.musclesNestedNodeMap.get(node);
+    const flatNode: FlatNode =
       existingNode && existingNode.item === node.item
         ? existingNode
-        : new MuscleFlatNode();
+        : new FlatNode();
     flatNode.item = node.item;
     flatNode.level = level;
     flatNode.expandable = !!node.children?.length;
-    this.flatNodeMap.set(flatNode, node);
-    this.nestedNodeMap.set(node, flatNode);
+    this.musclesFlatNodeMap.set(flatNode, node);
+    this.musclesNestedNodeMap.set(node, flatNode);
 
     return flatNode;
   };
 
-  async getData(): Promise<void> {
-    const snaps: firebase.default.firestore.QuerySnapshot<unknown> =
-      await this.db.collection("Muscles").get().toPromise();
+  propsTransformer = (node: Node, level: number) => {
+    const existingNode: FlatNode | undefined =
+      this.propsNestedNodeMap.get(node);
+    const flatNode: FlatNode =
+      existingNode && existingNode.item === node.item
+        ? existingNode
+        : new FlatNode();
+    flatNode.item = node.item;
+    flatNode.level = level;
+    flatNode.expandable = !!node.children?.length;
+    this.propsFlatNodeMap.set(flatNode, node);
+    this.propsNestedNodeMap.set(node, flatNode);
 
-    const muscles: MuscleNode[] = snaps.docs.map((snap) => {
+    return flatNode;
+  };
+
+  async getData(collection: string): Promise<Node[]> {
+    const snaps: firebase.default.firestore.QuerySnapshot<unknown> =
+      await this.db.collection(collection).get().toPromise();
+
+    const documents: Node[] = snaps.docs.map((snap) => {
       const snapData: Object = snap.data() as Object;
-      return <MuscleNode>{
+      return <Node>{
         item: snap.id,
         ...snapData,
       };
     });
 
-    this.muscleDb.dataChange.next(muscles || []);
+    return documents;
   }
 
-  getParentNode(node: MuscleFlatNode): MuscleFlatNode | null {
+  getParentNode(
+    node: FlatNode,
+    treeControl: FlatTreeControl<FlatNode>
+  ): FlatNode | null {
     const currentLevel: number = this.getLevel(node);
 
     if (currentLevel < 1) {
       return null;
     }
 
-    const startIndex: number = this.treeControl.dataNodes.indexOf(node) - 1;
+    const startIndex: number = treeControl.dataNodes.indexOf(node) - 1;
 
     for (let i: number = startIndex; i >= 0; i--) {
-      const currentNode: MuscleFlatNode = this.treeControl.dataNodes[i];
+      const currentNode: FlatNode = treeControl.dataNodes[i];
 
       if (this.getLevel(currentNode) < currentLevel) {
         return currentNode;
@@ -161,28 +213,72 @@ export class ConfigurationComponent implements OnInit {
     return null;
   }
 
-  addNode(node: MuscleFlatNode): void {
-    const parentNode: MuscleNode | undefined = this.flatNodeMap.get(node);
-    this.muscleDb?.insertItem(parentNode!, "");
-    this.treeControl.expand(node);
+  addNode(node: FlatNode, dbName: string): void {
+    switch (dbName) {
+      case "MusclesDb": {
+        const parentNode: Node | undefined = this.musclesFlatNodeMap.get(node);
+        this.muscleDb.insertItem(parentNode!, "");
+        this.musclesTreeControl.expand(node);
+        break;
+      }
+      case "PropsDb": {
+        const parentNode: Node | undefined = this.propsFlatNodeMap.get(node);
+        this.propsDb.insertItem(parentNode!, "");
+        this.propsTreeControl.expand(node);
+        break;
+      }
+    }
   }
 
-  saveNode(node: MuscleFlatNode, itemValue: string): void {
-    const nestedNode: MuscleNode | undefined = this.flatNodeMap.get(node);
-    this.muscleDb?.updateItem(nestedNode!, itemValue);
+  saveNode(node: FlatNode, itemValue: string, dbName: string): void {
+    switch (dbName) {
+      case "MusclesDb": {
+        const nestedNode: Node | undefined = this.musclesFlatNodeMap.get(node);
+        this.muscleDb.updateItem(nestedNode!, itemValue);
+        break;
+      }
+      case "PropsDb": {
+        const nestedNode: Node | undefined = this.propsFlatNodeMap.get(node);
+        console.log(nestedNode);
+        this.propsDb.updateItem(nestedNode, itemValue);
+        break;
+      }
+    }
   }
 
-  deleteNode(node: MuscleFlatNode): void {
-    const parentFlatNode: MuscleFlatNode | null = this.getParentNode(node);
-    const parentNode: MuscleNode | undefined = this.flatNodeMap.get(
-      parentFlatNode!
-    );
-    this.muscleDb.deleteItem(parentNode, node.item);
+  deleteNode(node: FlatNode, dbName: string): void {
+    switch (dbName) {
+      case "MusclesDb": {
+        const parentFlatNode: FlatNode | null = this.getParentNode(
+          node,
+          this.musclesTreeControl
+        );
+        const parentNode: Node | undefined = this.musclesFlatNodeMap.get(
+          parentFlatNode!
+        );
+        this.muscleDb.deleteItem(parentNode, node.item);
+        break;
+      }
+      case "PropsDb": {
+        const parentFlatNode: FlatNode | null = this.getParentNode(
+          node,
+          this.propsTreeControl
+        );
+        const parentNode: Node | undefined = this.propsFlatNodeMap.get(
+          parentFlatNode!
+        );
+        this.propsDb.deleteItem(parentNode, node.item);
+        break;
+      }
+    }
   }
 
-  onNewGroupClick(): void {
+  onNewMuscleGroupClick(): void {
     this.muscleDb.insertItem(null, "");
-    console.log(this.muscleDb.data);
+  }
+
+  onNewPropsGroupClick(): void {
+    this.propsDb.insertItem(null, "");
   }
 
   async onSaveClick(): Promise<void> {
@@ -200,6 +296,21 @@ export class ConfigurationComponent implements OnInit {
       musclesRef.ref.onSnapshot((snapshot) => {
         snapshot.docs.forEach((doc) => {
           musclesRef.doc(doc.id).delete();
+        });
+      });
+    }
+
+    const propsRef: AngularFirestoreCollection = this.db.collection("Props");
+
+    if (this.propsDb && this.propsDb.data.length > 0) {
+      for (const prop of this.propsDb.data) {
+        const propRef: AngularFirestoreDocument = propsRef.doc(prop.item);
+        await propRef.set(prop);
+      }
+    } else {
+      propsRef.ref.onSnapshot((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          propsRef.doc(doc.id).delete();
         });
       });
     }
