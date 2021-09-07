2c2273689c3cb40aa43182bd60a2590d18c84897
Dino Jakir <dino.jakir@eucon.com>
2021-09-07 14:02:53 +0200

    Exercise row expansion configured

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from "@angular/core";
 import { DbService } from "src/app/services/auth/db.service";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
+  animations: [
+    trigger("detailExpand", [
+      state("collapsed", style({ height: "0px", minHeight: "0" })),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      ),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
   exercises: any[] = [];
   displayedColumns: string[] = ["name", "type", "muscle"];
+  expandedElement: any;
   isLoadIndicatorVisible: boolean = false;
 
   constructor(private db: DbService) {}
@@ -17,5 +35,7 @@ export class HomeComponent implements OnInit {
     this.isLoadIndicatorVisible = true;
     this.exercises = await this.db.getCollectionDocuments("Exercises");
     this.isLoadIndicatorVisible = false;
+
+    console.log(this.exercises);
   }
 }
