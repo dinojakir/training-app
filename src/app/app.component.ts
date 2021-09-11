3d24f400c8b0a56aeb61200e07808ea4458b9654
Dino Jakir <dino.jakir@eucon.com>
2021-09-11 12:56:29 +0200

    Improvement for the navigation menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ElementRef, Renderer2, ViewChild } from "@angular/core";
 import { AuthService } from "./services/auth/auth.service";
 import { DxContextMenuComponent } from "devextreme-angular";
 import { NavigationEnd, Router } from "@angular/router";
@@ -11,11 +11,17 @@ import { NavigationEnd, Router } from "@angular/router";
 export class AppComponent {
   @ViewChild(DxContextMenuComponent, { static: true })
   contextMenu: DxContextMenuComponent | undefined;
+  @ViewChild("sidebar") sidebar: ElementRef | undefined;
+
   contextMenuItems = [{ id: 1, text: "Odjava", icon: "dx-icon-export" }];
   menuItems: any[];
   url: string = "";
 
-  constructor(public authService: AuthService, private router: Router) {
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private renderer: Renderer2
+  ) {
     this.menuItems = [
       { name: "Imenik", link: "/pocetna", icon: "dx-icon-bulletlist" },
       { name: "Vježba", link: "/vjezba", icon: "dx-icon-plus" },
@@ -25,6 +31,22 @@ export class AppComponent {
     router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.url = event.url;
+      }
+    });
+
+    this.renderer.listen("window", "click", (e: any) => {
+      if (
+        this.sidebar &&
+        e.target &&
+        !e.target.classList.contains("dx-icon-menu")
+      ) {
+        if (
+          window
+            .getComputedStyle(this.sidebar.nativeElement)
+            .getPropertyValue("z-index") === "1000"
+        ) {
+          this.sidebar.nativeElement.style.visibility = "hidden";
+        }
       }
     });
   }
@@ -44,5 +66,11 @@ export class AppComponent {
 
   async onUserBtnClick(): Promise<void> {
     await this.contextMenu?.instance.show();
+  }
+
+  expandMenu(): void {
+    if (this.sidebar) {
+      this.sidebar.nativeElement.style.visibility = "visible";
+    }
   }
 }
