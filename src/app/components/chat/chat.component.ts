16a761501f64aa7f852b61f7f7ed2d37288298aa
Dino Jakir <dino.jakir@eucon.com>
2021-10-18 18:05:04 +0200

    Chat configuration

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
 import { AuthService } from "src/app/services/auth/auth.service";
 import { DbService } from "src/app/services/auth/db.service";
 
@@ -9,9 +10,28 @@ import { DbService } from "src/app/services/auth/db.service";
 })
 export class ChatComponent implements OnInit {
   message: string = "";
-  constructor(public authService: AuthService, private db: DbService) {}
+  messages: any[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    public authService: AuthService,
+    private db: DbService,
+    public fireStore: AngularFirestore
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    this.messages = (await this.db.getCollectionDocuments("Messages")).sort(
+      (a, b) => (a.date - b.date <= 0 ? -1 : 1)
+    );
+
+    this.fireStore
+      .collection("Messages")
+      .snapshotChanges(["added", "removed", "modified"])
+      .subscribe(async () => {
+        this.messages = (await this.db.getCollectionDocuments("Messages")).sort(
+          (a, b) => (a.date - b.date <= 0 ? -1 : 1)
+        );
+      });
+  }
 
   async submit(): Promise<void> {
     if (this.message) {
@@ -19,10 +39,12 @@ export class ChatComponent implements OnInit {
       const user: string = this.authService?.user?.email
         .substring(0, 1)
         .toUpperCase();
+      const date: any = Date.now();
       const message: any = {
         user: user,
         message: this.message,
-        date: Date.now(),
+        date: date,
+        dateString: new Date(date).toUTCString(),
       };
       await this.db.saveCollectionDocument("Messages", message);
 
