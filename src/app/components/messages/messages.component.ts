d9949ebe254febb0119ebc4e4e14edb3cf1a8ab9
Dino Jakir <dino.jakir@eucon.com>
2021-10-26 18:06:08 +0200

    Improvement for sending a file in a message

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import emailjs, { EmailJSResponseStatus } from "emailjs-com";
+import { Component, OnInit } from "@angular/core";
 
 export class Message {
   title: string | undefined;
   email: string | undefined;
   text: string | undefined;
+  attachment: string | undefined;
 }
+
+declare var gapi: any;
 
 @Component({
   selector: "app-messages",
@@ -13,49 +15,101 @@ export class Message {
   styleUrls: ["./messages.component.scss"],
 })
 export class MessagesComponent implements OnInit {
+  file: any;
+  isSignedIn: boolean = false;
   message: Message = new Message();
+  attach: any;
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  public confirm(): void {
-    const forms: HTMLCollectionOf<HTMLFormElement> = document.forms;
-    const emailForm: HTMLFormElement | null = forms.namedItem("emailform");
-    if (emailForm) {
-      emailjs
-        .sendForm(
-          "service_howtorf",
-          "template_xnp97jr",
-          emailForm,
-          "user_fiElQ5Jb2gNhiUqYf9G3E"
-        )
-        .then(
-          (result: EmailJSResponseStatus) => {
-            console.log(result.text);
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-    }
+  ngOnInit(): void {
+    const _this: MessagesComponent = this;
+    gapi.load("client:auth2", _this.initClient);
   }
 
-  public send(e: Event): void {
-    emailjs
-      .sendForm(
-        "service_howtorf",
-        "template_xnp97jr",
-        e.target as HTMLFormElement,
-        "user_fiElQ5Jb2gNhiUqYf9G3E"
-      )
-      .then(
-        (result: EmailJSResponseStatus) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+  async onFileSelected(e: any): Promise<void> {
+    this.file = e.target.files[0];
+    this.message.attachment = this.file.name;
+
+    this.attach = await this.getBase64(this.file);
+  }
+
+  async confirm(): Promise<void> {
+    console.log(this.isSignedIn);
+    await gapi.auth2.getAuthInstance().signIn({
+      scope: "profile email",
+    });
+
+    await this.sendMessage();
+  }
+
+  async getBase64(file: any): Promise<string | ArrayBuffer | null> {
+    return new Promise((resolve, reject) => {
+      const reader: FileReader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  }
+
+  async initClient(): Promise<void> {
+    const apiKey: string = "AIzaSyARRbZw0JB7zqOXZETwhDR-z_4C8LoRuLE";
+    const clientId: string =
+      "860382123806-2tlpaergvsm1md9hha7ufff3jomrv6us.apps.googleusercontent.com";
+    const discoveryDocs: string[] = [
+      "https://gmail.googleapis.com/$discovery/rest?version=v1",
+    ];
+    const scopes: string = "https://mail.google.com/";
+
+    await gapi.client.init({
+      apiKey: apiKey,
+      discoveryDocs: discoveryDocs,
+      clientId: clientId,
+      scope: scopes,
+    });
+  }
+
+  async sendMessage(): Promise<void> {
+    const nl: string = "\n";
+    const boundary: string = "__myapp__";
+    const to: string = "dino.jakir.mobile@gmail.com";
+    const subject: string = "Hi";
+    const message: string = "hello";
+
+    const str: string = [
+      "MIME-Version: 1.0",
+      "Content-Transfer-Encoding: 7bit",
+      "to: " + to,
+      "subject: " + subject,
+      "Content-Type: multipart/alternate; boundary=" + boundary + nl,
+      "--" + boundary,
+      "Content-Type: text/plain; charset=UTF-8",
+      "Content-Transfer-Encoding: 7bit" + nl,
+      message + nl,
+      "--" + boundary,
+      "--" + boundary,
+      "Content-Type: Application/pdf; name=myPdf.pdf",
+      "Content-Disposition: attachment; filename=myPdf.pdf",
+      "Content-Transfer-Encoding: base64" + nl,
+      this.attach,
+      "--" + boundary + "--",
+    ].join("\n");
+
+    const encodedMessage: string = btoa(str);
+
+    const encodedMail: string = encodedMessage
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_");
+
+    await gapi.client.gmail.users.messages.send({
+      userId: "me",
+      raw: encodedMail,
+    });
+  }
+
+  async signoutClick(): Promise<void> {
+    await gapi.auth2.getAuthInstance().signOut();
+  }
+
+  updateSigninStatus(signedIn: boolean): void {
+    this.isSignedIn = signedIn;
   }
 }
