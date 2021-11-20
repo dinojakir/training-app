e35480c4a38c2c37d96cb7e5f7c8a8e187a4553b
Dino Jakir <dino.jakir@eucon.com>
2021-11-20 17:59:08 +0100

    New user form

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { DxFormComponent } from "devextreme-angular";
+
+export class User {
+  id: string | undefined;
+  name: string | undefined;
+  email: string | undefined;
+  password: string | undefined;
+}
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"],
 })
 export class UserComponent implements OnInit {
+  @ViewChild(DxFormComponent, { static: false }) form:
+    | DxFormComponent
+    | undefined;
 
-  constructor() { }
+  formValid: boolean | undefined = false;
+  user: User = new User();
 
-  ngOnInit(): void {
+  constructor() {
+    this.onNameChanged = this.onNameChanged.bind(this);
+    this.onEmailChanged = this.onEmailChanged.bind(this);
+    this.onPasswordChanged = this.onPasswordChanged.bind(this);
   }
 
+  ngOnInit(): void {}
+
+  onNameChanged() {
+    this.formValid = this.form?.instance.validate().isValid;
+  }
+
+  onEmailChanged() {
+    this.formValid = this.form?.instance.validate().isValid;
+  }
+
+  onPasswordChanged() {
+    this.formValid = this.form?.instance.validate().isValid;
+  }
+
+  confirm() {}
 }
