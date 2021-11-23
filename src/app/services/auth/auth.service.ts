2589f1a103c45471733585a393b1869180d3e5e0
Dino Jakir <dino.jakir@eucon.com>
2021-11-23 17:50:44 +0100

    Configuration of auth guard

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Router } from "@angular/router";
+import { Subject } from "rxjs";
 import { DbService } from "./db.service";
 
 @Injectable({
@@ -10,6 +11,7 @@ import { DbService } from "./db.service";
 export class AuthService {
   user: any;
   isAdmin: boolean = false;
+  status = new Subject<{ isLoggedIn: boolean; isAdmin: boolean }>();
 
   constructor(
     private db: DbService,
@@ -17,24 +19,42 @@ export class AuthService {
     public fireStore: AngularFirestore,
     public router: Router
   ) {
-    this.fireAuth.authState.subscribe((user) => {
+    this.fireAuth.authState.subscribe((user: any) => {
       if (user) {
         this.user = user;
         localStorage.setItem("korisnik", JSON.stringify(user));
+        this.isAdmin = user.authDomain === "training-app-1552e.firebaseapp.com";
+        this.status.next({ isLoggedIn: true, isAdmin: this.isAdmin });
+        this.router.navigate(["pocetna"]);
       } else {
         localStorage.removeItem("korisnik");
         this.user = undefined;
+        this.isAdmin = false;
+        this.status.next({ isLoggedIn: false, isAdmin: false });
         this.router.navigate(["prijava"]);
       }
     });
   }
 
   isLoggedIn(): boolean {
-    const user: string | null = localStorage.getItem("korisnik");
-    return user !== null;
+    const userStr: string | null = localStorage.getItem("korisnik");
+    if (userStr) {
+      const user = JSON.parse(userStr);
+
+      if (
+        !this.isAdmin &&
+        user.authDomain &&
+        user.authDomain === "training-app-1552e.firebaseapp.com"
+      ) {
+        this.isAdmin = true;
+        this.status.next({ isLoggedIn: true, isAdmin: this.isAdmin });
+      }
+    }
+
+    return userStr !== null;
   }
 
-  async signIn(email: any, password: any): Promise<boolean> {
+  async signIn(email: any, password: any): Promise<void> {
     const users: any = await this.db.getCollectionDocuments("Users");
     const user: any = users.find(
       (i: any) => i.email == email && i.password == password
@@ -43,25 +63,12 @@ export class AuthService {
       this.user = user;
       localStorage.setItem("korisnik", JSON.stringify(user));
       this.router.navigate(["pocetna"]);
-
-      return Promise.resolve(true);
     }
-    return this.fireAuth
+    this.fireAuth
       .signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        if (result.user) {
-          this.user = result.user;
-          this.isAdmin = true;
-          localStorage.setItem("korisnik", JSON.stringify(result.user));
-          this.router.navigate(["pocetna"]);
-        }
-
-        return true;
-      })
       .catch(async () => {
         this.user = undefined;
         localStorage.removeItem("korisnik");
-        return false;
       });
   }
 
@@ -69,6 +76,7 @@ export class AuthService {
     await this.fireAuth.signOut();
     this.user = undefined;
     localStorage.removeItem("korisnik");
+
     await this.router.navigate(["prijava"]);
   }
 }
